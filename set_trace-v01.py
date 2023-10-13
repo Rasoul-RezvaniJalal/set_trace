908acfafdd5b6bd1ce946b642e32f595852a7a74
Rasoul-RezvaniJalal <121818084+Rasoul-RezvaniJalal@users.noreply.github.com>
2023-10-13 12:09:48 +0330

    Update set_trace-v01.py

--- set_trace-v01.py
+++ set_trace-v01.py
@@ -1,169 +1,199 @@
 import sys
 from math import sqrt
+import math
 import pandas as pd
-
-linenos_nonfaulty = {}
-linenos_faulty = {}
-linenos_sum = {}
-exec_result = {}
-Ochiai_suspiciousness = {}
-Tarantula_suspiciousness = {}
-total_failed = 0
-total_passed = 0
-line_temp = []
-whole_activities = []
-# test_cases_test = [[1,0],[6,12],[3,2],[9,18],[12,24],[0,-1]]
-test_cases_getImpact =[[[4,1,3],4],[[7,6,3],7],[[6,3,3],6],[[2,1,3],2],[[3,2,3],3],[[9,7,3],9],[[8,3,2],(8/3)],[[9,2,2],(9/2)],[[6,8,3],8],[[7,6,1],13],[[6,12,3],12],[[8,9,3],9]]
+import os
 
 
-def traceit(frame, event, arg):
+class execution_path():
+    def __init__(self):
+        self.linenos_nonfaulty = {}  # none faulty line number
+        self.linenos_faulty = {}    # faulty line number
+        self.linenos_sum = {}
+        self.exec_result = []
+        self.Ochiai_suspiciousness = {} # every line suspiciousness score by Ochiai formula
+        self.Tarantula_suspiciousness = {}  # every line suspiciousness score by Tarantula formula
+        self.total_failed = 0
+        self.total_passed = 0
+        self.line_temp = []
+        self.whole_activities = []
+        self.test_case_alpha4 = [[[4, 4, 8, 9, 10], 1, "f"], [[4, 5, 8, 1, 7], 0, "t"], [[1, 6, 4, 3, 2], 1, "t"],
+                                 [[3, 3, 4, 3, 3], 1, "t"], [[8, 7, 6, 5, 4], 1, "f"], [[7, 7, 7, 3, 7], 1, "t"],
+                                 [[7, 8, 9, 4, 2], 0, "t"]]  # test suite for alpha4 function
 
-    if event == "line":
+    def traceit(self, frame, event, arg):
+        if event == "line":
 
-        lineno = frame.f_lineno
+            lineno = frame.f_lineno
 
-        line_temp.append(lineno)
+            if lineno not in self.line_temp:
+                self.line_temp.append(lineno)
 
-        if lineno in linenos_sum:
-            
-            linenos_sum[lineno] +=1
-        else:
-            linenos_sum[lineno] = 1
+            if lineno in self.linenos_sum:
 
-    return traceit
-
-
-def getImpact(a,b,c):
-    res = 0
-    div = 1
-    sum = a+b
-    if (a > 0) and (b > 0):
-        div = a/b
-    max = b
-    if a > b:
-        max = b
-    if c == 1:
-        res = sum
-    if c == 2:
-        res = div
-    if c == 3:
-        res = max
-    return res
-# def test(x):
-#     if(x>5):
-#         return(2*x)
-#     else:
-#         return(x-2)
-
-
-def Tarantula(line_number,passed,failed):
-    Tarantula_suspiciousness[line_number] = (failed/total_failed) / ((failed/total_failed) + (passed/total_passed))
-
-
-def Ochiai(line_number,passed,failed):
-
-    Ochiai_suspiciousness[line_number] = failed / sqrt(total_failed*(passed+failed))
-
-
-sys.settrace(traceit)
-
-"""for i in test_cases_test:   ----------------------------->   uncomment for check the "test" function  -----
-    result = test(i[0])
-    if(i[1] == result):
-        total_passed+=1
-        for j in line_temp:
-            if j in linenos_nonfaulty:
-
-                linenos_nonfaulty[j] += 1
+                self.linenos_sum[lineno] += 1
             else:
-                linenos_nonfaulty[j] = 1
-    else:
-        total_failed+=1
-        for j in line_temp:
-            if j in linenos_faulty:
+                self.linenos_sum[lineno] = 1
 
-                linenos_faulty[j] += 1
+        return self.traceit
+
+    def trace_back(self, frame, event, arg):  # neutralize "traceit" effects
+        if event == "line":
+            pass
+        return self.trace_back
+
+    @staticmethod
+    def alpha4(array: list, type: int):
+        arrayLenght = len(array)
+        is_sorted = True
+        sum = 0
+        sum_square = 0
+        Average = 0
+        arr_norm_sd = list()
+        arr_norm = list()
+        for i in range(0, arrayLenght):
+            arr_norm_sd.append(array[i])
+            arr_norm.append(array[i])
+        for i in range(0, len(arr_norm_sd) - 1):
+            if arr_norm_sd[i] < arr_norm_sd[i + 1]:
+                is_sorted = False
+                break
+        if not is_sorted:
+            for i in range(0, arrayLenght - 1):
+                for j in range(0, arrayLenght - i - 1):
+                    if (arr_norm[j] > arr_norm[j + 1]):
+                        temp = arr_norm[j]
+                        arr_norm[j] = arr_norm[j + 1]
+                        arr_norm[j + 1] = temp
+            Range = arr_norm[arrayLenght - 1] - arr_norm[0]
+            for i in range(0, arrayLenght):
+                sum = sum + arr_norm_sd[i]
+            avrage = sum / arrayLenght
+            for number in arr_norm_sd:
+                sum_square = sum_square + ((number - avrage) * (number - avrage))
+            SD = math.sqrt(sum_square / (arrayLenght - 1))
+            if type == 0:
+                for i in (0, arrayLenght - 1):
+                    arr_norm_sd[i] = ((array[i] - avrage) / SD)
+                return arr_norm_sd
             else:
-                linenos_faulty[j] = 1
-    line_temp = []"""
-ind = 1
-for i in test_cases_getImpact:
-    result = getImpact(i[0][0], i[0][1], i[0][2])
+                for i in range(0, arrayLenght):
+                    arr_norm[i] = array[i] - array[0] / (Range + 1)
+                return arr_norm
 
-    if i[1] == result:
-        exec_result[ind] = 1
-        total_passed += 1
-        for j in line_temp:
-            if j in linenos_nonfaulty:
+    def Tarantula(self, line_number, passed, failed):
+        self.Tarantula_suspiciousness[line_number] = (failed / self.total_failed) / (
+                (failed / self.total_failed) + (passed / self.total_passed))
 
-                linenos_nonfaulty[j] += 1
+    def Ochiai(self, line_number, passed, failed):
+        self.Ochiai_suspiciousness[line_number] = failed / sqrt(self.total_failed * (passed + failed))
+
+    def extract_info(self):
+        sys.settrace(self.traceit)
+        ind = 1
+        for i in self.test_case_alpha4:
+            result = i[2]
+            self.alpha4(array=i[0], type=i[1])
+
+            if (result == "t"):
+                # exec_result[ind] = 1
+                self.exec_result.append("1")
+                self.total_passed += 1
+                for j in self.line_temp:
+                    if j in self.linenos_nonfaulty:
+
+                        self.linenos_nonfaulty[j] += 1
+                    else:
+                        self.linenos_nonfaulty[j] = 1
+
             else:
-                linenos_nonfaulty[j] = 1
+                self.exec_result.append("0")
+                self.total_failed += 1
+                for h in self.line_temp:
+                    if h in self.linenos_faulty:
 
-    else:
-        exec_result[ind] = 0
-        total_failed += 1
-        for h in line_temp:
-            if h in linenos_faulty:
+                        self.linenos_faulty[h] += 1
+                    else:
+                        self.linenos_faulty[h] = 1
+            self.whole_activities.append(self.line_temp)
+            self.line_temp = []
+            ind += 1
+        print(self.whole_activities)
+        print(self.linenos_sum)
+        sys.settrace(self.trace_back)  # neutralize "traceit" effects
+        first_line = min(self.linenos_sum)
+        last_line = max(self.linenos_sum)
+        col_numbers = len(self.test_case_alpha4)
+        rows_number = last_line - first_line + 1
 
-                linenos_faulty[h] += 1
-            else:
-                linenos_faulty[h] = 1
-    whole_activities.append(line_temp)
-    line_temp = []
-    ind += 1
-rows_number = len(linenos_sum)
-col_numbers = len(test_cases_getImpact)
-li = sorted(linenos_sum.items())
-first_line = li[0][0]
+        for i in self.linenos_sum.copy():
+            passed_no = 0
+            failed_no = 0
+            if i in self.linenos_nonfaulty:
+                passed_no = self.linenos_nonfaulty[i]
 
-for i in linenos_sum.copy():
-    passed_no = 0
-    failed_no = 0
-    if i in linenos_nonfaulty:
-        passed_no = linenos_nonfaulty[i]
+            if i in self.linenos_faulty:
+                failed_no = self.linenos_faulty[i]
 
-    if i in linenos_faulty:
-        failed_no = linenos_faulty[i]
+            self.Ochiai(i, passed_no, failed_no)
+            self.Tarantula(i, passed_no, failed_no)
 
-    Ochiai(i,passed_no,failed_no)
-    Tarantula(i,passed_no,failed_no)
+        self.matrix = []
+
+        for i in range(rows_number + 1):
+            new = []
+            for j in range(col_numbers + 1):
+                new.append(0)
+            self.matrix.append(new)
+
+        for m in range(1, col_numbers + 1):
+            self.matrix[0][m] = m
+        self.matrix[0][0] = 0
+
+        for n in range(rows_number):
+            self.matrix[n + 1][0] = first_line + n
+
+        ind = 1
+        for i in self.whole_activities:
+            for j in i:
+
+                for k in self.matrix:
+                    if (k[0] == j):
+                        ex_ind = j - (first_line - 1)
+                        self.matrix[ex_ind][ind] = 1
+            ind += 1
+
+        self.exec_result.insert(0, "result")
+        self.matrix[0].append("tarantula")
+        self.matrix[0].append("Ochiai")
+
+        for i, j in zip(range(first_line, first_line + rows_number), range(rows_number)):
+            if i in self.Tarantula_suspiciousness:
+                self.matrix[j + 1].append(self.Tarantula_suspiciousness[i])
+                self.matrix[j + 1].append(self.Ochiai_suspiciousness[i])
+        self.matrix.append(self.exec_result)
+        self.df = pd.DataFrame(self.matrix).to_string(index=True)
+
+    def export_data(self):
+        pd.DataFrame(self.matrix).to_excel("output_matrix.xlsx")
+        pd.DataFrame(self.matrix).to_latex("output_matrix.tex")
+        print(self.df,
+              f"\n------------------------------\n \n 0 : means fault\n\n------------------------------\nEXPLANATION : \ncolumns show line numbers \n and \nrows show test_data numbers\n 1 : means related line number execute with the test_data \n 0 : means related line number does NOT execute with the test_data  \n------------------------------\n")
+
+        print("number of time that each line has appeared in faulty and non_faulty execution has shown below : \n")
+        print(
+            f"NONE_FAULTY : \n {sorted(self.linenos_nonfaulty.items())} \n\n FAULTY : \n {sorted(self.linenos_faulty.items())}\n\n")
+
+    def runner(self):
+        self.extract_info()
+        self.export_data()
 
 
-
-matrix = []
-
-for i in range(col_numbers+1):
-    new = []
-    for j in range(rows_number+1):
-        new.append(0)
-    matrix.append(new)
-
-for m in range(rows_number):
+def main():
+    instance = execution_path()
+    instance.runner()
+    print('end of the process')
 
 
-    matrix[0][m+1] = first_line + m
-matrix[0][0] = 0
-
-for n in range (1,col_numbers+1):
-    matrix[n][0] = n
-
-
-ind = 1
-for i in whole_activities:
-    for j in i:
-
-            if(j in matrix[0]):
-
-                ex_ind = j - (first_line-1)
-                matrix[ind][ex_ind] = 1
-    ind += 1
-
-df = pd.DataFrame(matrix).to_string(index=False)
-
-pd.DataFrame(matrix).to_csv("out.csv")
-print(df,f"\n------------------------------\nresult for each test_data is : {exec_result} \n 0 : means fault\n\n------------------------------\nEXPLANATION : \ncolumns show line numbers \n and \nrows show test_data numbers\n 1 : means related line number execute with the test_data \n 0 : means related line number does NOT execute with the test_data  \n------------------------------\n")
-
-print("number of time that each line appeared in faulty and non_faulty execution has shown below : \n")
-print(f"NONE_FAULTY : \n {sorted(linenos_nonfaulty.items())} \n\n FAULTY : \n {sorted(linenos_faulty.items())}\n\n")
-print(f" the ochiai suspiciousness is : \n\n {sorted(Ochiai_suspiciousness.items())} \n\n\n and the Tarantula suspiciousness is :\n\n {sorted(Tarantula_suspiciousness.items())} \n")
+if __name__ == '__main__':
+    main()
